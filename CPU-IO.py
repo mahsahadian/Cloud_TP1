acb7452ee0ade53b3aaace37598028817e54a80b
openja moses <openjamosesopm@gmail.com>
2020-02-15 14:48:59 -0500

    CPU and IO code

---
+++ CPU-IO.py
@@ -0,0 +1,14 @@
+def readCPU(option, output_path, fileName):
+    command = "sysbench --test=cpu --cpu-max-prime=" + str(option) + " run"
+    path = output_path + "" + fileName + ".txt"
+    file = open(path, 'a')
+    subprocess.call(command, shell=True, stdout=file)
+    print(command)
+    file.close()
+
+def readIO(bs, count, output_path, fileName):
+    command = "dd if=/dev/zero of=sb-io-test bs=" + str(bs) + " count=" + str(c$
+    path = output_path + "/" + fileName + ".txt"
+    file = open(path, 'a')
+    subprocess.call(command, shell=True, stderr=file)
+    file.close()
